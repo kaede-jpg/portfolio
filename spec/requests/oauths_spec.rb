17b48ccc46120c28a995cde5e91ea93a6b285594
kaede-jpg <happymaple428@gmail.com>
2024-05-12 00:14:45 +0900

    ローカル環境のSSL化以外のLINEログイン機能を実装

--- spec/requests/oauths_spec.rb
+++ spec/requests/oauths_spec.rb
@@ -1,26 +1,24 @@
 require 'rails_helper'
 
-=begin
-RSpec.describe 'Oauths', type: :request do
-  describe 'GET /:provider' do
-    it 'returns http success' do
-      get '/oauths/:provider'
-      expect(response).to have_http_status(:success)
-    end
-  end
-
-  describe 'GET /callback' do
-    it 'returns http success' do
-      get '/oauths/callback'
-      expect(response).to have_http_status(:success)
-    end
-  end
-
-  describe 'POST /callback' do
-    it 'returns http success' do
-      post '/oauths/callback'
-      expect(response).to have_http_status(:success)
-    end
-  end
-end
-=end
+# RSpec.describe 'Oauths', type: :request do
+#   describe 'GET /:provider' do
+#     it 'returns http success' do
+#       get '/oauths/:provider'
+#       expect(response).to have_http_status(:success)
+#     end
+#   end
+#
+#   describe 'GET /callback' do
+#     it 'returns http success' do
+#       get '/oauths/callback'
+#       expect(response).to have_http_status(:success)
+#     end
+#   end
+#
+#   describe 'POST /callback' do
+#     it 'returns http success' do
+#       post '/oauths/callback'
+#       expect(response).to have_http_status(:success)
+#     end
+#   end
+# end
