4b7b14f67ed2a93f075ac4e5f0e89bf5a2085766
Kaede <happymaple428@gmail.com>
2024-05-12 20:14:38 +0900

    LINEログイン機能の修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,5 +8,6 @@ class User < ApplicationRecord
   validates :password, confirmation: true, if: -> { new_record? || changes[:crypted_password] }
   validates :password_confirmation, presence: true, if: -> { new_record? || changes[:crypted_password] }
 
-  validates :email, uniqueness: true
+  validates :email, presence: true, uniqueness: true
+  validates :name, presence: true
 end
