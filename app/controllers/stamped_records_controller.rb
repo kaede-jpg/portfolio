b9c8abcbd58bb66d76b913a596a17515917e9aac
kaede-jpg <happymaple428@gmail.com>
2024-06-09 11:47:11 +0900

    スタンプ表示の調整

--- app/controllers/stamped_records_controller.rb
+++ app/controllers/stamped_records_controller.rb
@@ -1,8 +1,10 @@
 class StampedRecordsController < ApplicationController
   def create
     @stamped_record = StampedRecord.new(record_id: params[:record_id], stamp_id: params[:stamp_id])
-    return if @stamped_record.save
-
-    render status: :unprocessable_entity
+    if @stamped_record.save
+      @stamped_records = StampedRecord.where(record_id: params[:record_id], stamp_id: params[:stamp_id])
+    else
+      render status: :unprocessable_entity
+    end
   end
 end
