d34aef9a7e962a088c06e4a5446535ddb5f2a55d
kaede-jpg <happymaple428@gmail.com>
2024-06-19 08:00:12 +0900

    本番環境のgmail利用設定

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -71,11 +71,21 @@ Rails.application.configure do
   # config.active_job.queue_adapter = :resque
   # config.active_job.queue_name_prefix = "app_production"
 
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    port: 587,
+    address:"smtp.gmail.com",
+    domain: 'gmail.com',
+    user_name: Rails.application.credentials.dig(Rails.env.to_sym,:gmail, :address),
+    password: Rails.application.credentials.dig(Rails.env.to_sym,:gmail, :password),
+    authentication: :plain,
+    enable_starttls_auto: true
+  }
   config.action_mailer.perform_caching = false
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
-  # config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.raise_delivery_errors = true
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
