06be432664204dab0211ad1b200f318950444a0b
kaede-jpg <happymaple428@gmail.com>
2024-07-11 23:59:26 +0900

    スコア機能を追加

--- app/helpers/records_helper.rb
+++ app/helpers/records_helper.rb
@@ -1,2 +1,23 @@
 module RecordsHelper
+  def score(records)
+    sum = 0
+    count = 0
+    records.each do |record|
+      sum += record.each_score
+      count += 1
+    end
+    count.zero? ? 0 : sum / count
+  end
+
+  def score_comment(score)
+    if score.zero?
+      'まだみんなにスタンプをもらっていないようです。'
+    elsif score > 70
+      '１日頑張れたみたい！'
+    elsif score > 40
+      'まあまあな１日を過ごせたみたい。'
+    else
+      'あんまり頑張れなかったみたい…'
+    end
+  end
 end
