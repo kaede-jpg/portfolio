ede26c0e70c684b3b7a5c96457613281dbb4d7b2
kaede-jpg <156039137+kaede-jpg@users.noreply.github.com>
2024-07-23 18:32:49 +0900

    Revert "画像をwebp形式に置き換え、記録投稿時、webp形式・リサイズしてから保存するよう修正"

--- app/controllers/guest_logins_controller.rb
+++ app/controllers/guest_logins_controller.rb
@@ -1,9 +1,25 @@
 class GuestLoginsController < ApplicationController
   skip_before_action :require_login, only: %i[create]
   def create
-    create_guest_user
-    auto_login(@guest_user)
-    create_sample_record(@guest_user)
+    guest_user = User.create(
+      name: 'ゲスト',
+      email: "#{SecureRandom.alphanumeric(10)}@email.com",
+      password: 'password',
+      password_confirmation: 'password',
+      role: 'monitored',
+      guest: true,
+      agreement: true
+    )
+    guest_user.activate!
+    auto_login(guest_user)
+    record = guest_user.records.build
+    record.meal_image.attach(
+      io: Rails.root.join('app/assets/images/sample_meal.jpg').open,
+      filename: 'sample_meal.jpg',
+      content_type: 'image/jpg'
+    )
+    record.save!
+    MealAdviseJob.perform_later(record)
     redirect_to records_path, notice: 'ゲストとしてログインしました'
   end
 
@@ -14,31 +30,5 @@ class GuestLoginsController < ApplicationController
       current_user.monitor!
     end
     redirect_to records_path
-  end
-
-  private
-  
-  def create_guest_user
-    @guest_user = User.create(
-      name: 'ゲスト',
-      email: "#{SecureRandom.alphanumeric(10)}@email.com",
-      password: 'password',
-      password_confirmation: 'password',
-      role: 'monitored',
-      guest: true,
-      agreement: true
-    )
-    @guest_user.activate!
-  end
-
-  def create_sample_record(user)
-    record = user.records.build
-    record.meal_image.attach(
-      io: Rails.root.join('app/assets/images/sample_meal.webp').open,
-      filename: 'sample_meal.webp',
-      content_type: 'image/webp'
-    )
-    record.save!
-    MealAdviseJob.perform_later(record)
   end
 end
