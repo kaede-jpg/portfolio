ff19d59c291f8b1a12d34b231982c7067193d7cc
kaede-jpg <156039137+kaede-jpg@users.noreply.github.com>
2024-07-23 20:27:21 +0900

    Revert "guest_logins_controller.rbのコンフリクト修正漏れ"

--- app/controllers/guest_logins_controller.rb
+++ app/controllers/guest_logins_controller.rb
@@ -28,17 +28,25 @@ class GuestLoginsController < ApplicationController
       guest: true,
       agreement: true
     )
-   @guest_user.activate!
-  end
-  
-  def create_sample_record(user)
-    record = user.records.build
+    guest_user.activate!
+    auto_login(guest_user)
+    record = guest_user.records.build
     record.meal_image.attach(
-      io: Rails.root.join('app/assets/images/sample_meal.webp').open,
-      filename: 'sample_meal.webp',
-      content_type: 'image/webp'
+      io: Rails.root.join('app/assets/images/sample_meal.jpg').open,
+      filename: 'sample_meal.jpg',
+      content_type: 'image/jpg'
     )
     record.save!
     MealAdviseJob.perform_later(record)
+    redirect_to records_path, notice: 'ゲストとしてログインしました'
+  end
+
+  def change_role
+    if current_user.monitor?
+      current_user.monitored!
+    else
+      current_user.monitor!
+    end
+    redirect_to records_path
   end
 end
